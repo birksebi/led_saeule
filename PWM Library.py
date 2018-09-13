73f44abfee88d88643b3da218a2ad5285204ccb5
birksebi <birksebi@gmail.com>
2018-09-13 21:44:51 +0200

    Initial Commit#2

--- PWM Library.py
+++ PWM Library.py
@@ -1,11 +1,19 @@
 import RPi.GPIO as GPIO
-
+import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 
 p =GPIO.PWM(12, 400000)
 p.start(0)
-try:
-    while 1:
-        
+while:
+    green[]=
+    red[]=
+    blue[]=
+    greenbin[]=bin(green[])
+p.ChangeDutyCycle(wert)
+    #3=0
+    #7=1
+
+p.stop()
+GPIO.cleanup()
